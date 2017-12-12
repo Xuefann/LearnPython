3e49bfc7dd4037dcc15d6f24499c2e5bbc6136b6
Rosso_Dino <fantasia8727@gmail.com>
2017-12-12 19:28:42 +0800

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 
-	You are given a text, which contains different english letters and punctuation symbols. You should find the most frequent letter in the text. The letter returned must be in lower case.
-While checking for the most wanted letter, casing does not matter, so for the purpose of your search, "A" == "a". Make sure you do not count punctuation symbols, digits and whitespaces, only letters.
-If you have two or more letters with the same frequency, then return the letter which comes first in the latin alphabet. For example -- "one" contains "o", "n", "e" only once for each, thus we choose "e".
+	You are given a text, which contains different english letters and punctuation symbols. You should 
+find the most frequent letter in the text. The letter returned must be in lower case.While checking for the
+most wanted letter, casing does not matter, so for the purpose of your search, "A" == "a". Make sure you do
+not count punctuation symbols, digits and whitespaces, only letters.If you have two or more letters with the
+same frequency, then return the letter which comes first in the latin alphabet. For example -- "one" contains
+"o", "n", "e" only once for each, thus we choose "e".
 	Input: A text for analysis as a string.
 	Output: The most frequent letter in lower case as a string.
 	Precondition:
